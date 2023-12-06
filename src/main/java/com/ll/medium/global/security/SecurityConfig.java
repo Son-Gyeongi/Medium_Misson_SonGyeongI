8f91f5e8ec3cc171f0b796f8306bb2dbc7574454
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-06 15:44:18 +0900

    feat : 패스워드 암호화

--- src/main/java/com/ll/medium/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium/global/security/SecurityConfig.java
@@ -2,10 +2,14 @@ package com.ll.medium.global.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true) // 로그인 여부를 판별하기 위해 사용했던 @PreAuthorize 애너테이션을 사용하기 위해 반드시 필요하다.
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -29,5 +33,12 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    // 패스워드 암호화
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        // BCrypt 해싱 함수(BCrypt hashing function)를 사용해서 비밀번호를 암호화
+        return new BCryptPasswordEncoder();
     }
 }
