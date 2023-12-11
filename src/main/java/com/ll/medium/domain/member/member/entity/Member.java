d773fbe55e027c7edc9f252e4c253c3ea1a91c24
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-12 01:21:54 +0900

    refactor : 엔티티 변경 - Member, Post 엔티티에 생성 날짜, 수정 날짜 추가 - Member, Post와 겹치는 필드의 경우 BaseEntity를 만들어서 상속

--- src/main/java/com/ll/medium/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium/domain/member/member/entity/Member.java
@@ -1,25 +1,21 @@
 package com.ll.medium.domain.member.member.entity;
 
+import com.ll.medium.global.jpa.baseEntity.BaseEntity;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.List;
 
 @Entity
-@Getter
-@Builder
+@SuperBuilder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @EqualsAndHashCode.Include // 값 비교시 id만 비교
-    private Long id;
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class Member extends BaseEntity {
     private String username;
     private String password;
 
@@ -28,6 +24,7 @@ public class Member {
     }
 
     // 권한 부여
+    @SuppressWarnings("JpaAttributeTypeInspection") // 무시해도 되는 오류를 끌 수 있다.
     public List<SimpleGrantedAuthority> getAuthorities() {
         if (isAdmin()) {
             return List.of(new SimpleGrantedAuthority(Role.ADMIN.value()),
