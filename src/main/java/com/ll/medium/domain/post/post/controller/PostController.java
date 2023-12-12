dc2ecfe00613d9164f388011cd214899c028befa
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-12 21:27:26 +0900

    feat : PUT /post/{id}/modify : {id}번 글 수정 폼 처리 - 수정을 하고 비공개인 경우 main으로 이동

--- src/main/java/com/ll/medium/domain/post/post/controller/PostController.java
+++ src/main/java/com/ll/medium/domain/post/post/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.ll.medium.domain.post.post.controller;
 
 import com.ll.medium.domain.post.post.entity.Post;
+import com.ll.medium.domain.post.post.form.ModifyForm;
 import com.ll.medium.domain.post.post.form.WriteForm;
 import com.ll.medium.domain.post.post.service.PostService;
 import com.ll.medium.global.rq.Rq;
@@ -49,7 +50,7 @@ public class PostController {
     // 게시글 상세내용
     @GetMapping("/{id}")
     public String detailPost(@PathVariable Long id, Model model) {
-        Post post = postService.getPost(id);
+        Post post = postService.getDetailPost(id);
 
         model.addAttribute("post", post);
 
@@ -65,6 +66,28 @@ public class PostController {
         model.addAttribute("post", post);
 
         return "domain/post/post/modify";
+    }
+
+    // 게시글 수정 POST
+    @PreAuthorize("isAuthenticated()")
+    @PutMapping("{id}/modify")
+    public String modifyPost(@Valid ModifyForm modifyForm, @PathVariable Long id) {
+        // 게시글 존재 여부
+        Post post = postService.getPost(id);
+
+        // 권한 여부 체크
+        if (!postService.canModify(rq.getMember(), post)) {
+            throw new RuntimeException("수정 권한이 없습니다.");
+        }
+
+        // 게시글 수정
+        postService.modify(post, modifyForm.getTitle(), modifyForm.getBody(), modifyForm.getIsPublished(), rq.getMember());
+
+        if (modifyForm.getIsPublished() == Boolean.TRUE) {
+            return rq.redirect("/post/%d".formatted(id), "%d번 게시글이 수정되었습니다.".formatted(id));
+        } else {
+            return rq.redirect("/", "%d번 게시글이 수정되었습니다.".formatted(id));
+        }
     }
 
     // 게시글 삭제
--- src/main/java/com/ll/medium/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/medium/domain/post/post/service/PostService.java
@@ -9,11 +9,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostService {
     private final PostRepository postRepository;
     private final Rq rq;
@@ -25,6 +27,7 @@ public class PostService {
         return posts;
     }
 
+    @Transactional
     public RsData<Post> write(String title, String body, Boolean isPublished, Member author) {
         Post post = new Post(title, body, isPublished, author);
 
@@ -46,9 +49,25 @@ public class PostService {
 
     // 게시글 가져오기
     public Post getPost(Long id) {
+        return postRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 하는 게시글이 없습니다.")
+        );
+    }
+
+    // 게시글 상세 조회
+    public Post getDetailPost(Long id) {
         return postRepository.findByIsPublishedTrueAndId(id).orElseThrow(
                 () -> new IllegalArgumentException("해당 하는 게시글이 없습니다.")
         );
+    }
+
+    // 게시글 수정
+    @Transactional
+    public void modify(Post post, String title, String body, Boolean isPublished, Member author) {
+        post.setTitle(title);
+        post.setBody(body);
+        post.setAuthor(author);
+        post.setIsPublished(isPublished);
     }
 
     // 게시글 삭제
@@ -57,6 +76,12 @@ public class PostService {
     }
 
     // 권한 여부 체크는 서비스에서 해야함
+    public boolean canModify(Member author, Post post) {
+        if (author == null) return false;
+
+        return post.getAuthor().equals(author);
+    }
+
     public boolean canDelete(Member author, Post post) {
         if (author == null) return false;
 
